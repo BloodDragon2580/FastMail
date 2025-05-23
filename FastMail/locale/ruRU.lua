8ab53586edb880890be643e42c1a2dda52564bb2
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-05-24 04:54:51 +0500

    Create ruRU.lua

---
+++ FastMail/locale/ruRU.lua
@@ -0,0 +1,12 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("FastMail", "ruRU")
+if not L then return end
+
+L["NONE"] = "Нет писем."
+L["NEW"] = "Новое/непрочитанное письмо!"
+L["OLD"] = "Непрочитанные письма."
+L["ATLEAST"] = "По крайней мере %s писем от:"
+L["SENDERS"] = ""
+L["STATS"] = "Статистика"
+L["UNREAD"] = "Непрочитано: "
+L["READ"] = "Прочитано: "
+L["KNOWN"] = "Известно: "
